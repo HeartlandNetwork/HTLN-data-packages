4fc02105305aa2b6051a69b95e067526ae2b6a0d
HeartlandNetwork <gareth_rowell@nps.gov>
2024-01-23 16:19:22 -0600

    substrate and spacing

---
+++ fish/src/TransectSpacingIntervalQC.R
@@ -0,0 +1,61 @@
+
+library(tidyverse)
+
+transectspacing <- read_csv("./TransectSpacingInterval.csv")
+
+problems(transectspacing) 
+
+view(transectspacing)
+
+glimpse(transectspacing)
+
+
+# Variable: Parkname -----------------------------------------------------------
+
+# Unique list - PASS?
+
+transectspacing |> distinct(ParkName)
+
+
+# Variable: ParkCode -----------------------------------------------------------
+
+# Unique list - PASS
+
+transectspacing |> distinct(ParkCode)
+
+ggplot(transectspacing, aes(x = ParkCode)) + 
+  geom_bar()
+
+# Variables: LocationID, PeriodID and EventID ----------------------------------
+
+transectspacing |> distinct(LocationID) |> 
+  print(n = 80)
+
+transectspacing |> distinct(PeriodID) |> 
+  print(n = 74)
+
+transectspacing |> distinct(EventID) |> 
+  print(n = 327)
+
+
+# Variables: ChannelType, ChannelTypeDesc, ChannelTypeSampledLength_m
+#  NumberofTransects, TransectSpacingInverval_m
+
+ transectspacing |> distinct(ChannelType)
+ 
+ transectspacing |> distinct(ChannelTypeDescr)
+ 
+ transectspacing |> distinct(ChannelTypeSampledLength_m) |>
+   print(n = 81)
+ 
+ transectspacing |> distinct(NumberOfTransects)
+ 
+ transectspacing |> distinct(TransectSpacingInterval_m) |>
+   print(n = 65)
+ 
+ 
+ 
+ 
+ 
+ 
+ 
